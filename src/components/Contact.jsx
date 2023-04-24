f9f49cd05775ced155f57a216c8ea68b7623efbf
Luciano Coronel <86379664+lucho9595@users.noreply.github.com>
2023-04-24 16:54:22 -0300

    map and contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,19 +1,53 @@
 import React from "react";
 import styled from "styled-components";
+import Map from "./Map";
 
 const Section4 = styled.div`
 height: 100vh;
 scroll-snap-align: center;
 .container{
     width: 100%;
+    height: 100%;
     display: flex;
     justify-content: space-between;
     gap: 50px;
     .left{
         flex: 1;
         display: flex;
-        align-items: center;
-        justify-content: center;
+        justify-content: flex-end;
+        align-items: center;        
+    .form{
+            width: 500px;
+            display: flex;
+            flex-direction: column;
+            gap: 25px;
+            .title{
+                .subtitle{
+                    font-weight: 200;
+                }
+            }
+            .input{
+                padding: 20px;
+                background-color: lightgray;
+                border: none;
+                border-radius: 5px;
+            }
+            .textarea{
+                padding: 20px;
+                background-color: lightgray;
+                border: none;
+                border-radius: 5px;
+            }
+            .btn{
+                background-color: #da4ea2;
+                color: white;
+                border: none;
+                font-weight: bold;
+                cursor: pointer;
+                border-radius: 5px;
+                padding: 20px;
+            }
+        } 
     }
     .right{
         flex: 1;
@@ -33,13 +67,13 @@ const Contact = () => {
                         <input className="input" placeholder="Name"></input>
                         <input className="input" placeholder="Email"></input>
                         <input className="input" placeholder="Phone Number"></input>
-                        <textarea className="textarea" placeholder="Write your message"></textarea>
+                        <textarea className="textarea" placeholder="Write your message" rows={10}></textarea>
                         <button className="btn">Send</button>
 
                     </form>
                 </div>
                 <div className="right">
-
+                    <Map />
                 </div>
             </div>
         </Section4>
